ec6fb2af41f7de6223b99123b4ecd5f369baf2f0
onur mat <onurmatik@gmail.com>
2026-05-13 10:39:13 +0300

    Brand Django admin with Solfasol Kooperatifi

--- solfasol/urls.py
+++ solfasol/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for solfasol project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
@@ -24,6 +8,10 @@ from coop import views as coop_views
 from members.forms import EmailOrUsernameAuthenticationForm
 from members import views as member_views
 from solfasol.api import api
+
+admin.site.site_header = "Solfasol Kooperatifi"
+admin.site.site_title = "Solfasol Kooperatifi"
+admin.site.index_title = "Solfasol Kooperatifi"
 
 urlpatterns = [
     path('', coop_views.dashboard, name='dashboard'),
